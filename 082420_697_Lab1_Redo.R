0f096cabb8d68219e660ad6c063e90efedf4e0a5
Olivia Macrorie <omacrorie@umaass.edu>
2020-09-06 16:31:11 -0400

    Lab 1

---
+++ 082420_697_Lab1_Redo.R
@@ -0,0 +1,32 @@
+#Exercise 1
+```{r}
+x <- 3
+y <- 15
+x + y
+x - y
+x * y
+x / y
+```
+
+#Exercise 2
+```{r}
+ x <- 2
+y <- 3
+x^5 > y^4
+```
+
+#Exercise 3  
+```{r}
+ a <- c(22,62,148,43,29)
+b <- sum (a)
+a / b
+```
+
+
+#Exercise 4
+```{r}
+ d <- c("A","T","C","G")
+sort(d)
+```
+
+
